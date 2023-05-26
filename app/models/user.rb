ec4ef01db136f4e7d5a4f9cf1c5ff214888f74ec
Akash1298 <akashrana0153@gmail.com>
2023-05-26 16:54:14 +0530

    Added User resource

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  validates :username, presence:true, length: {minimum:3, maximum:15},  uniqueness: { case_sensitive: false }
+  has_secure_password
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(username: "Akash", password: "123456")
+User.create(username: "Vishvendra", password: "123456")
+User.create(username: "Amit", password: "123456")
+User.create(username: "Darshil", password: "123456")
+User.create(username: "Vikas", password: "123456")
