0100b2881e578bcbe75988c48c6578981102a416
Akash1298 <akashrana0153@gmail.com>
2023-05-26 17:06:39 +0530

    added message resource and message partial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_26_111052) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_26_112521) do
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
