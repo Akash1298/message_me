6ccd1a09a069271ca5c24fb2d207d0b6dd094cc6
Akash1298 <akashrana0153@gmail.com>
2023-05-26 19:18:47 +0530

    added authentication on ui and controller level

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "chatroom#index"
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
