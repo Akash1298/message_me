f0e0c25cf9010056bec1f7e093284115071c0daa
Akash1298 <akashrana0153@gmail.com>
2023-05-27 04:50:43 +0530

    added post message feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'messages/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
 end
